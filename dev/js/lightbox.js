a4d417aa92e308e2d3a9da056d3ffbb11b5801bb
alexandr camp ar <alejand2124@gmail.com>
2018-01-06 00:56:12 -0500

    creando lightbox en js primera parte

--- dev/js/lightbox.js
+++ dev/js/lightbox.js
@@ -1,1 +1,44 @@
-let num= 0;
+// al hacer click en una imagem se abra su ventana grande
+const getImage = container => [...container.querySelectorAll('img')];
+const getLargeImagenes = gallery => gallery
+    .map(el => el.src)
+    .map(el => el.replace('min','large'));
+const getDescriptions = gallery=> gallery.map(el=>el.alt);
+const openLightBoxEvent = (container,gallery) =>{
+    container.addEventListener('click', e =>{
+        let el = e.target,
+            i= gallery.indexOf(el);
+        if(el.tagName === 'IMG'){
+            openLightbox(gallery,i,large,descriptions);
+        }
+    });
+};
+//console.log(getLargeImagenes(getImage(document.querySelector('.gallery-container'))));
+let openLightbox = (gallery,i,large,descriptions) =>{
+    //abrir lightbox al pulsar el parametro image
+    let lightboxElement = document.createElement('div');
+    lightboxElement.innerHTML= `
+    <div class="lightbox-overlay">
+       <figure class="lightbox-container">
+          <img src="${large[i]}" class="lightbox-image">
+          <figcaption>
+            <p class="lightbox-description">${descriptions[i]}</p>
+            <nav class="lightbox-navigation">
+              <a href="#" class="lightbox-navigation__button prev"></a>
+              <span class="lightbox-navigation__counter">Imagen ${i+1} de ${gallery.length}</span>
+              <a href="#" class="lightbox-navigation__button next"></a>
+            </nav>
+          </figcaption>
+       </figure> 
+    </div>`;
+    lightboxElement.id = 'lightbox';
+    document.body.appendChild(lightboxElement);
+};
+
+
+//se abre en un overlay
+// ese overlay debe poder cerrarse
+
+//al abrirse la version grande debe  tener lo siguiente
+//descripcion de la imagem(que se tomara del atributo)
+// navegacion entre la imagen siguiente
--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,3 +1,44 @@
-"use strict";
+'use strict';
 
-var num = 0;
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+// al hacer click en una imagem se abra su ventana grande
+var getImage = function getImage(container) {
+    return [].concat(_toConsumableArray(container.querySelectorAll('img')));
+};
+var getLargeImagenes = function getLargeImagenes(gallery) {
+    return gallery.map(function (el) {
+        return el.src;
+    }).map(function (el) {
+        return el.replace('min', 'large');
+    });
+};
+var getDescriptions = function getDescriptions(gallery) {
+    return gallery.map(function (el) {
+        return el.alt;
+    });
+};
+var openLightBoxEvent = function openLightBoxEvent(container, gallery) {
+    container.addEventListener('click', function (e) {
+        var el = e.target,
+            i = gallery.indexOf(el);
+        if (el.tagName === 'IMG') {
+            openLightbox(gallery, i, large, descriptions);
+        }
+    });
+};
+//console.log(getLargeImagenes(getImage(document.querySelector('.gallery-container'))));
+var openLightbox = function openLightbox(gallery, i, large, descriptions) {
+    //abrir lightbox al pulsar el parametro image
+    var lightboxElement = document.createElement('div');
+    lightboxElement.innerHTML = '\n    <div class="lightbox-overlay">\n       <figure class="lightbox-container">\n          <img src="' + large[i] + '" class="lightbox-image">\n          <figcaption>\n            <p class="lightbox-description">' + descriptions[i] + '</p>\n            <nav class="lightbox-navigation">\n              <a href="#" class="lightbox-navigation__button prev"></a>\n              <span class="lightbox-navigation__counter">Imagen ' + (i + 1) + ' de ' + gallery.length + '</span>\n              <a href="#" class="lightbox-navigation__button next"></a>\n            </nav>\n          </figcaption>\n       </figure> \n    </div>';
+    lightboxElement.id = 'lightbox';
+    document.body.appendChild(lightboxElement);
+};
+
+//se abre en un overlay
+// ese overlay debe poder cerrarse
+
+//al abrirse la version grande debe  tener lo siguiente
+//descripcion de la imagem(que se tomara del atributo)
+// navegacion entre la imagen siguiente
