cbde9a575e1f20ee1e71c5e7930fe46c4fc21c91
alexandr camp ar <alejand2124@gmail.com>
2018-01-06 11:28:47 -0500

    avanzando lightbox mostrando en view modal

--- dev/js/lightbox.js
+++ dev/js/lightbox.js
@@ -1,44 +1,100 @@
-// al hacer click en una imagem se abra su ventana grande
-const getImage = container => [...container.querySelectorAll('img')];
-const getLargeImagenes = gallery => gallery
+// al hacer click en una imagen se abra su version grande
+// obtener la galeria de imagenes
+const  getImagenes= container =>[...container.querySelectorAll('img')];
+// Obtener un array de las rutas de las imagenes grandes
+const  getLargeImagenes = gallery => gallery
     .map(el => el.src)
-    .map(el => el.replace('min','large'));
-const getDescriptions = gallery=> gallery.map(el=>el.alt);
-const openLightBoxEvent = (container,gallery) =>{
-    container.addEventListener('click', e =>{
+    .map(el =>el.replace('min','large'));
+// obtener las descripciones de las imagenes
+const getDescripcion = gallery => gallery.map(el => el.alt);
+// capturar el eveneto click en la galeria para abrir el lightbox
+const  openLightboxEvenet = (container,gallery,larges,descripciones)=>{
+    container.addEventListener('click',e =>{
         let el = e.target,
-            i= gallery.indexOf(el);
+            i = gallery.indexOf(el);
         if(el.tagName === 'IMG'){
-            openLightbox(gallery,i,large,descriptions);
+            openLightBox(gallery,i,larges,descripciones);
         }
     });
 };
-//console.log(getLargeImagenes(getImage(document.querySelector('.gallery-container'))));
-let openLightbox = (gallery,i,large,descriptions) =>{
-    //abrir lightbox al pulsar el parametro image
+// imprimir overlay del lightbox en el bodys
+const openLightBox = (gallery,i,larges,descripciones) =>{
+    //abrir lightbox al pulsar el parametro de la imagen
     let lightboxElement = document.createElement('div');
-    lightboxElement.innerHTML= `
-    <div class="lightbox-overlay">
-       <figure class="lightbox-container">
-          <img src="${large[i]}" class="lightbox-image">
-          <figcaption>
-            <p class="lightbox-description">${descriptions[i]}</p>
-            <nav class="lightbox-navigation">
-              <a href="#" class="lightbox-navigation__button prev"></a>
-              <span class="lightbox-navigation__counter">Imagen ${i+1} de ${gallery.length}</span>
-              <a href="#" class="lightbox-navigation__button next"></a>
-            </nav>
-          </figcaption>
-       </figure> 
-    </div>`;
-    lightboxElement.id = 'lightbox';
+    lightboxElement.innerHTML=`
+          <div class="lightbox-overlay">
+            <figure class="lightbox-container">
+              <div class="close-modal">&#88;</div>
+              <img src="${larges[i]}" height="500px" width="950px">
+              <figcaption>
+                 <p class="lightbox-descripcion">${descripciones[i]}</p>
+                 <nav class="lightbox-navigation">
+                    <a href="#" class="lightbox-navigation_button prev">&#9194;</a>
+                    <span class="lightbox-navigation_counter">Imagen ${i+1} de ${gallery.length} </span>
+                    <a href="#" class="lightbox-navigation_button next">&#9193;</a>
+                 </nav>
+              </figcaption>
+            </figure>
+          </div>
+    `;
+    lightboxElement.id='lightbox';
     document.body.appendChild(lightboxElement);
+    closeModal(lightboxElement);
+    navigateLightbox(lightboxElement,i,larges,descripciones);
 };
 
+const navigateLightbox = (lightboxElement,i,larges,descripciones)=>{
+    let prevButton = lightboxElement.querySelector('.prev'),
+        nextButton = lightboxElement.querySelector('.next'),
+        image= lightboxElement.querySelector('img'),
+        description= lightboxElement.querySelector('p'),
+        counter=lightboxElement.querySelector('span'),
+        closeButton= lightboxElement.querySelector('.close-modal');
+    window.addEventListener('keyup', e => {
+        if(e.key === 'ArrowRight') nextButton.click();
+        if(e.key ==='ArrowLeft') prevButton.click();
+        if(e.key === 'Escape') closeButton.click();
+    });
+    lightboxElement.addEventListener('click', e =>{
+        e.preventDefault();
+        let target =e.target;
+        if(target=== prevButton){
+            if(i>0){
+                image.src=larges[i - 1];
+                i--;
+            }else{
+                image.src= larges[larges.length - 1];
+                i= larges.length-1;
+            }
 
-//se abre en un overlay
-// ese overlay debe poder cerrarse
+        }else if(target===nextButton){
+            if(i< larges.length -1){
+                image.src=larges[i+1];
+                i++;
+            }else{
+                image.src= larges[0];
+                i=0;
+            }
 
-//al abrirse la version grande debe  tener lo siguiente
-//descripcion de la imagem(que se tomara del atributo)
-// navegacion entre la imagen siguiente
+        }
+        description.textContent= descripciones[i];
+        counter.textContent= `Imagen ${i+1} de ${larges.length}`;
+
+    });
+};
+
+const closeModal = modalElement => {
+    let closeModal = modalElement.querySelector('.close-modal');
+    closeModal.addEventListener('click', e => {
+        e.preventDefault();
+        document.body.removeChild(modalElement);
+    });
+};
+
+const lightbox = container => {
+    let images= getImagenes(container),
+        large=getLargeImagenes(images),
+        descriptions=getDescripcion(images);
+    openLightboxEvenet(container,images,large,descriptions);
+};
+lightbox(document.getElementById('gallery-container'));
--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -2,10 +2,12 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-// al hacer click en una imagem se abra su ventana grande
-var getImage = function getImage(container) {
+// al hacer click en una imagen se abra su version grande
+// obtener la galeria de imagenes
+var getImagenes = function getImagenes(container) {
     return [].concat(_toConsumableArray(container.querySelectorAll('img')));
 };
+// Obtener un array de las rutas de las imagenes grandes
 var getLargeImagenes = function getLargeImagenes(gallery) {
     return gallery.map(function (el) {
         return el.src;
@@ -13,32 +15,82 @@ var getLargeImagenes = function getLargeImagenes(gallery) {
         return el.replace('min', 'large');
     });
 };
-var getDescriptions = function getDescriptions(gallery) {
+// obtener las descripciones de las imagenes
+var getDescripcion = function getDescripcion(gallery) {
     return gallery.map(function (el) {
         return el.alt;
     });
 };
-var openLightBoxEvent = function openLightBoxEvent(container, gallery) {
+// capturar el eveneto click en la galeria para abrir el lightbox
+var openLightboxEvenet = function openLightboxEvenet(container, gallery, larges, descripciones) {
     container.addEventListener('click', function (e) {
         var el = e.target,
             i = gallery.indexOf(el);
         if (el.tagName === 'IMG') {
-            openLightbox(gallery, i, large, descriptions);
+            openLightBox(gallery, i, larges, descripciones);
         }
     });
 };
-//console.log(getLargeImagenes(getImage(document.querySelector('.gallery-container'))));
-var openLightbox = function openLightbox(gallery, i, large, descriptions) {
-    //abrir lightbox al pulsar el parametro image
+// imprimir overlay del lightbox en el bodys
+var openLightBox = function openLightBox(gallery, i, larges, descripciones) {
+    //abrir lightbox al pulsar el parametro de la imagen
     var lightboxElement = document.createElement('div');
-    lightboxElement.innerHTML = '\n    <div class="lightbox-overlay">\n       <figure class="lightbox-container">\n          <img src="' + large[i] + '" class="lightbox-image">\n          <figcaption>\n            <p class="lightbox-description">' + descriptions[i] + '</p>\n            <nav class="lightbox-navigation">\n              <a href="#" class="lightbox-navigation__button prev"></a>\n              <span class="lightbox-navigation__counter">Imagen ' + (i + 1) + ' de ' + gallery.length + '</span>\n              <a href="#" class="lightbox-navigation__button next"></a>\n            </nav>\n          </figcaption>\n       </figure> \n    </div>';
+    lightboxElement.innerHTML = '\n          <div class="lightbox-overlay">\n            <figure class="lightbox-container">\n              <div class="close-modal">&#88;</div>\n              <img src="' + larges[i] + '" height="500px" width="950px">\n              <figcaption>\n                 <p class="lightbox-descripcion">' + descripciones[i] + '</p>\n                 <nav class="lightbox-navigation">\n                    <a href="#" class="lightbox-navigation_button prev">&#9194;</a>\n                    <span class="lightbox-navigation_counter">Imagen ' + (i + 1) + ' de ' + gallery.length + ' </span>\n                    <a href="#" class="lightbox-navigation_button next">&#9193;</a>\n                 </nav>\n              </figcaption>\n            </figure>\n          </div>\n    ';
     lightboxElement.id = 'lightbox';
     document.body.appendChild(lightboxElement);
+    closeModal(lightboxElement);
+    navigateLightbox(lightboxElement, i, larges, descripciones);
 };
 
-//se abre en un overlay
-// ese overlay debe poder cerrarse
+var navigateLightbox = function navigateLightbox(lightboxElement, i, larges, descripciones) {
+    var prevButton = lightboxElement.querySelector('.prev'),
+        nextButton = lightboxElement.querySelector('.next'),
+        image = lightboxElement.querySelector('img'),
+        description = lightboxElement.querySelector('p'),
+        counter = lightboxElement.querySelector('span'),
+        closeButton = lightboxElement.querySelector('.close-modal');
+    window.addEventListener('keyup', function (e) {
+        if (e.key === 'ArrowRight') nextButton.click();
+        if (e.key === 'ArrowLeft') prevButton.click();
+        if (e.key === 'Escape') closeButton.click();
+    });
+    lightboxElement.addEventListener('click', function (e) {
+        e.preventDefault();
+        var target = e.target;
+        if (target === prevButton) {
+            if (i > 0) {
+                image.src = larges[i - 1];
+                i--;
+            } else {
+                image.src = larges[larges.length - 1];
+                i = larges.length - 1;
+            }
+        } else if (target === nextButton) {
+            if (i < larges.length - 1) {
+                image.src = larges[i + 1];
+                i++;
+            } else {
+                image.src = larges[0];
+                i = 0;
+            }
+        }
+        description.textContent = descripciones[i];
+        counter.textContent = 'Imagen ' + (i + 1) + ' de ' + larges.length;
+    });
+};
 
-//al abrirse la version grande debe  tener lo siguiente
-//descripcion de la imagem(que se tomara del atributo)
-// navegacion entre la imagen siguiente
+var closeModal = function closeModal(modalElement) {
+    var closeModal = modalElement.querySelector('.close-modal');
+    closeModal.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.removeChild(modalElement);
+    });
+};
+
+var lightbox = function lightbox(container) {
+    var images = getImagenes(container),
+        large = getLargeImagenes(images),
+        descriptions = getDescripcion(images);
+    openLightboxEvenet(container, images, large, descriptions);
+};
+lightbox(document.getElementById('gallery-container'));
